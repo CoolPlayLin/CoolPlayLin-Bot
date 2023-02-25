1aa6ab345ff0e3c3f2b89d1dac6ff24e5fa92ed4
CoolPlayLin <CoolPlayLin@qq.com>
2023-02-25 18:44:56 +0800

    更新：准备接入第三方API

--- api/util.py
+++ api/util.py
@@ -6,7 +6,7 @@ from .typings import TaskManagerExit
 
 __all__ = ("TaskManager", "Logger")
 
-DefaultJSON = {"Root": None, "Admin": [], "BotQQ": None,"NotAllowUser":[], "BadWords": [], "AcceptPort": 5120, "PostIP": "127.0.0.1:5700", "@Me": None, "AdminGroup": []}
+DefaultJSON = {"Root": None, "Admin": [], "BotQQ": None,"NotAllowUser":[], "BadWords": [], "AcceptPort": 5120, "PostIP": "127.0.0.1:5700", "@Me": None, "AdminGroup": [], "Keys":{"Weather":None}}
 
 class TaskManager:
     __slots__ = ("Perform_QueuingTask", "Perform_RunningTask", "Status", "TaskLimit")
@@ -45,11 +45,12 @@ class TaskManager:
 
 FileLock = Lock()
 
-def JsonAuto(Json:dict, Action:str, PATH:Path) -> bool|dict:
+def JsonAuto(Json:dict, Action:str, PATH:Path) -> any:
     if not PATH.exists():
         with FileLock:
             with open(PATH, "w+", encoding="utf-8") as f:
                 f.write(json.dumps(DefaultJSON))
+    FileName = PATH.stem+PATH.suffix
     if Action == "WRITE":
         try:
             with FileLock:
